996231801e58c2fada8a9cee51d835e985062f19
mukthy <muktheeswaran.m@gmail.com>
2021-12-07 06:00:32 +0530

    band generator program completed - day 1

---
+++ band_generator.py
@@ -0,0 +1,6 @@
+# a simple band-generator program using your city name and petname
+print("Welcome to Band Name Generator\n")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+#bandname = city + pet
+print("Your band name could be " + city + "-" + pet)
