faab2dd9bf36e80caad52f1840053afbeabf2a85
mukthy <muktheeswaran.m@gmail.com>
2022-01-27 03:34:41 +0530

    Created FizBuzz Game

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+from cgitb import reset
+
+
+for num in range(1, 101):
+    if (num % 3 == 0 and num % 5 == 0):
+        num = 'FizzBuzz'
+    elif (num % 3 == 0):
+        num = 'Fizz'
+    elif (num % 5 == 0):
+        num = 'Buzz'
+    print(num)
