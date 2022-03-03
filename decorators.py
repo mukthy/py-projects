db5c4d7dcdcbac592d727091273a9d31f4684dd1
mukthy <muktheeswaran.m@gmail.com>
2022-03-03 17:59:32 +0530

    Decorators

--- decorators.py
+++ decorators.py
@@ -1,6 +1,13 @@
 # it usually have @ symbol at the start. It is possible because these functions acts as variables.
 
-# A function which accepts another function is a higher order function.
+# A function which accepts another function as a parameter or returns a function is a higher order function.
 
 def great(func):
     func()
+
+
+def greet2():
+    def func():
+        return 5
+    return func
+# filter(), map() are also decorators
