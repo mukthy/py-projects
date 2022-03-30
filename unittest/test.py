caf50e18f508187b6cbf95160a1eececdb0248b7
mukthy <muktheeswaran.m@gmail.com>
2022-03-31 05:29:37 +0530

    updated

--- unittest/test.py
+++ unittest/test.py
@@ -3,6 +3,10 @@ import main
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("Testing is starting")
+
     def test_something(self):
         test_param = 10
         result = main.do_stuff(test_param)
@@ -13,6 +17,24 @@ class MyTestCase(unittest.TestCase):
         result = main.do_stuff(test_param)
         # self.assertTrue(isinstance(result, ValueError))  # add assertion here
         self.assertIsInstance(result, ValueError)
+
+    def test_something3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        # self.assertTrue(isinstance(result, ValueError))  # add assertion here
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_something4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        # self.assertTrue(isinstance(result, ValueError))  # add assertion here
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_something5(self):
+        test_param = 0
+        result = main.do_stuff(test_param)
+        # self.assertTrue(isinstance(result, ValueError))  # add assertion here
+        self.assertEqual(result, 'Please enter a number')
 
 
 if __name__ == '__main__':
