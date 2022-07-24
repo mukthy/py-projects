a1b10644ef0e5b2b40202978c41df73d0500b912
mukthy <muktheeswaran.m@gmail.com>
2022-07-24 17:56:45 +0530

    Created new object, Instansiate

--- realpython/class_tut.py
+++ realpython/class_tut.py
@@ -6,3 +6,20 @@ class Dog:  # class name is dog
         self.name = name  # instance attributes
         self.age = age  # instance attributes
         # self is used to refer the current object being created during instantiation
+
+
+# create a new variable called philo and assign it to a new object, instantiating the Dog class
+
+
+philo = Dog('Philo', 5)
+mikey = Dog('Mikey', 6)
+
+mikey.age = 7
+philo.species = 'fish'
+
+# access the name and age attributes of the Dog object
+
+print("{} is {} and {} is {}".format(philo.name, philo.age, mikey.name, mikey.age))
+
+if philo.species == 'mammal':
+    print("{} is a {}".format(philo.name, philo.species))
