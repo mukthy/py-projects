bfdbf179bea12a44d702fcc47684abad39ef7ec0
mukthy <muktheeswaran.m@gmail.com>
2022-08-10 08:34:50 +0530

    Fast API Notes

---
+++ realpython/fastapi/fastapi_restapi.py
@@ -0,0 +1,17 @@
+# FASTAPI
+# FastAPI is a Python framework for building blazing-fast APIs, it is written with creating REST Web APIs in mind.
+
+# REST = REpresentational State Transfer
+# Employs the HTTP protocol so it does not need a special platform to host the API.
+# Uses the existing HTTP methods GET, POST, PUT and DELETE, it maps the CRUD functions create, read, update and delete to the HTTP methods.
+# Access via an HTTP endpoint / web address.
+# www.python.com/api/users
+# Endpoints return an HTTP response with JSON.
+# Reduces the noise associated with other REST Frameworks.
+# SuperFast, Rivals for node.js and Go.
+# Fewer Errors
+# Less time to develop
+# Uses open standards such as OpenAPI and JSON Schema.
+# Employs Modern Python Features
+# 1. Type Hinting
+# 2. Async without requiring knowledge of asyncio in the standard library.
---
+++ realpython/fastapi/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException
+from mongita import MongitaClientDisk
+from pydantic import BaseModel
+
+
+class Shape(BaseModel):
+    name: str
+    no_of_sides: int
+    id: int
+
+
+# shapes = [
+#     {"item_name": "Circle", "no_of_sides": 1, "id": 1},
+#     {"item_name": "Square", "no_of_sides": 4, "id": 2},
+#     {"item_name": "Triangle", "no_of_sides": 3, "id": 3},
+# ]
+
+app = FastAPI()  # create an instance of FastAPI class
+client = MongitaClientDisk()  # create an instance of MongitaClientDisk class
+db = client.db  # create an instance of MongitaClientDisk class
+shapes = db.shapes  # collection name is shapes
+
+
+@app.get("/")  # decorator to map the endpoint to the function
+async def root():  # function to return the response
+    return {"message": "Hello World"}
+
+
+# uvicorn main:app --host "0.0.0.0" --port 8000 --reload
+
+@app.get("/shapes")  # decorator to map the endpoint to the function
+async def get_shapes():  # function to return the response
+    existing_shapes = shapes.find({})  # find all the shapes in the collection
+    return [
+        {key: shape[key] for key in shape if key != "_id"}
+        for shape in existing_shapes
+    ]
+
+
+@app.get("/shapes/{shape_id}")  # decorator to map the endpoint to the function
+async def get_shapes_by_id(shape_id: int):  # function to return the response
+    # for shape in shapes:
+    #     if shape['id'] == shape_id:
+    #         return shape
+    if shapes.count_documents({"id": shape_id}) > 0:
+        shape = shapes.find_one({"id": shape_id})
+        return {key: shape[key] for key in shape if key != "_id"}
+
+    raise HTTPException(status_code=404, detail=f"No Shape with {shape_id} found")
+
+
+@app.post("/shapes")  # decorator to map the endpoint to the function
+async def post_shape(shape: Shape):  # function to return the response
+    shapes.insert_one(shape.dict())
+    return shape
